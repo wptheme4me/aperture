30a2efd75a022a90a22016e9992bf57212d00feb
unknown <hey@wptheme4.me>
2018-11-05 14:21:54 +0200

    first commit

---
+++ config.js
@@ -0,0 +1,7 @@
+module.exports = {
+    site_title: "Fotostudija aperture.lt",
+    dev_siteurl: "http://localhost:3000",
+    prod_siteurl: "https://aperture-lt.netlify.com/",
+    google_analytics_key: "UA-108132847-2",
+    style_sheet: "/css/style.css",
+}

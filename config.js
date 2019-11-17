c89eae5ad15c960366396fdac9f12e5d922b7e34
wptheme4me <32770934+wptheme4me@users.noreply.github.com>
2019-11-17 09:27:28 +0200

    Google analytics tag

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ module.exports = {
     site_title: "Fotostudija aperture.lt",
     dev_siteurl: "http://localhost:3000",
     prod_siteurl: "https://www.aperture.lt/",
-    google_analytics_key: "UA-108132847-2",
+    google_analytics_key: "UA-152746481-1",
     facebook_appid: "187624092172947",
     style_sheet: "/css/style.css",
 }
